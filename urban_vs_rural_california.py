6e864a4bde2250bc573f6aabec87443ed9e6b778
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-04-21 15:40:40 -0400

    Update urban_vs_rural_california.py

--- urban_vs_rural_california.py
+++ urban_vs_rural_california.py
@@ -39,8 +39,8 @@ with tab2:
 
 with tab3:
     with st.container():
-    st.image('https://cdn.cookielaw.org/logos/abdd0205-22cc-4fe3-9905-15c572527293/7556293c-5b79-4ada-b220-2a9a79bf5b49/be7ee31a-0ca2-4aeb-894e-ee0219934cbe/okcupid_whitebg.png')
-    col1, col2 = st.columns(2)
+      st.image('https://cdn.cookielaw.org/logos/abdd0205-22cc-4fe3-9905-15c572527293/7556293c-5b79-4ada-b220-2a9a79bf5b49/be7ee31a-0ca2-4aeb-894e-ee0219934cbe/okcupid_whitebg.png')
+      col1, col2 = st.columns(2)
     with col1:
       st.header('Visualization')
     with col2:
