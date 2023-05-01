b9b204e7ac70e0ec491799d8a07b2102d78922d6
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-05-01 16:45:10 -0400

    Update urban_vs_rural_california.py

--- urban_vs_rural_california.py
+++ urban_vs_rural_california.py
@@ -18,6 +18,18 @@ chart1 = alt.Chart(data).mark_bar().encode(
     selection
 )
 
+all_sex = data.sex.unique().tolist()
+
+selected = st.multiselect(
+    "Programming languages", options=all_sex, default=all_sex)
+
+plot_df = data[data.sex.isin(all_sex)]
+
+chart2 = alt.Chart(plot_df).mark_arc(innerRadius=40).encode(
+    theta="religion",
+    color="religion:N",
+    tooltip = []
+)
 st.set_page_config(page_title="Urban vs Rural California", initial_sidebar_state = 'collapsed', layout = 'wide')
 
 #Can we scroll? Controls
@@ -37,7 +49,7 @@ with tab1:
     st.image('https://cdn.cookielaw.org/logos/abdd0205-22cc-4fe3-9905-15c572527293/7556293c-5b79-4ada-b220-2a9a79bf5b49/be7ee31a-0ca2-4aeb-894e-ee0219934cbe/okcupid_whitebg.png')
   with st.container():
     st.markdown(""" <style> .big-font { font-size:45px !important } </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="big-font">  Females have better vocabulary than males</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">  Do females have better vocabulary than males?</p>', unsafe_allow_html=True)
   with st.container():
     st.write("This project seeks to answer whether or not women have a better vocabulary than men. I go about this using the OKCupid dataset that was released in May 2016. The data was collected in between November 2014 to May 2015 by a group of Danish researchers. It is a very big dataset with around 59,000 rows of data. This was too much to work with so I did some major trimming of the dataset. The essay-type questions were split up into nine different categories, so I removed rows that had any unfilled essays in any of the nine categories and combined them all into a full essay category. For the religion column, I just wanted the user’s religion and not how they felt about it, so I split it in a way that only gives the religion. ")
     
