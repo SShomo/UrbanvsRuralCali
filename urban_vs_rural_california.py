a05e6117546078202a55febf5cec017120bb2f25
S_Shomo <90357187+SShomo@users.noreply.github.com>
2023-05-01 22:55:07 -0400

    Update urban_vs_rural_california.py

--- urban_vs_rural_california.py
+++ urban_vs_rural_california.py
@@ -112,10 +112,10 @@ with tab3:
 
       word_cloud = WordCloud(width=2000,height=2000, max_words=800,relative_scaling=0, background_color='white').generate_from_frequencies(all_words)
 
-      e = plt.imshow(word_cloud, interpolation='bilinear')
+      plt.imshow(word_cloud, interpolation='bilinear')
       plt.axis("off")
       plt.show()
-      st.pyplot(e)
+      st.pyplot()
       #st.write("This is a test")
     with col2:
       st.header('Visualization')
